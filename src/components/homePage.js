e45e235f3624479352d37be0ebc9b67fb1060422
vitorsei <seiji.vitor@gmail.com>
2016-08-04 22:18:42 -0300

    First React component

---
+++ src/components/homePage.js
@@ -0,0 +1,22 @@
+/**
+ * Created by VitorSeiji on 04/08/2016.
+ */
+
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>React, React Router, and Flux for ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
+
+
