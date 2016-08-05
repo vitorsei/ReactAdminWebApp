d6cb74a90dacdde1f57ebe98d4d22a7ea540041a
vitorsei <seiji.vitor@gmail.com>
2016-08-05 00:22:15 -0300

    Demo: Centralized Header

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -29700,6 +29700,34 @@ module.exports = About;
 
 },{"react":157}],159:[function(require,module,exports){
 /**
+ * Created by VitorSeiji on 05/08/2016.
+ */
+
+"use strict";
+
+var React = require('react');
+
+var Header = React.createClass({displayName: "Header",
+    render: function() {
+        return (
+            React.createElement("nav", {className: "navbar navbar-default"}, 
+                React.createElement("div", {className: "container-fluid"}, 
+                    React.createElement("a", {href: "/", className: "navbar-brand"}, 
+                        React.createElement("img", {src: "images/pluralsight-logo.png"})
+                    ), 
+                    React.createElement("ul", {className: "nav navbar-nav"}, 
+                        React.createElement("li", null, React.createElement("a", {href: "/"}, "Home")), 
+                        React.createElement("li", null, React.createElement("a", {href: "/#about"}, "About"))
+                    )
+                )
+            )
+        );
+    }
+});
+
+module.exports = Header;
+},{"react":157}],160:[function(require,module,exports){
+/**
  * Created by VitorSeiji on 04/08/2016.
  */
 
@@ -29719,11 +29747,12 @@ var Home = React.createClass({displayName: "Home",
 });
 
 module.exports = Home;
-},{"react":157}],160:[function(require,module,exports){
+},{"react":157}],161:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
 (function(win) {
     "use strict";
@@ -29738,6 +29767,7 @@ var About = require('./components/about/aboutPage');
 
             return (
                 React.createElement("div", null, 
+                    React.createElement(Header, null), 
                     React.createElement(Child, null)
                 )
             );
@@ -29753,4 +29783,4 @@ var About = require('./components/about/aboutPage');
     window.addEventListener('hashchange', render);
     render();
 })(window);
-},{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":2,"react":157}]},{},[160]);
+},{"./components/about/aboutPage":158,"./components/common/header":159,"./components/homePage":160,"jquery":2,"react":157}]},{},[161]);
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
 (function(win) {
     "use strict";
@@ -16,6 +17,7 @@ var About = require('./components/about/aboutPage');
 
             return (
                 <div>
+                    <Header/>
                     <Child/>
                 </div>
             );
