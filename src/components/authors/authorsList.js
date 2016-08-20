02c705c3c075adcf9971d84fabdbbab1ce518ca6
vitorsei <seiji.vitor@gmail.com>
2016-08-20 14:18:33 -0300

    AuthorPage

---
+++ src/components/authors/authorsList.js
@@ -0,0 +1,38 @@
+/**
+ * Created by VitorSeiji on 20/08/2016.
+ */
+
+"use strict";
+
+var React = require('react');
+
+var AuthorsList = React.createClass({
+
+    render: function () {
+        var createAuthorRow = function (author) {
+            return (
+                <tr key={author.id}>
+                    <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
+                    <td>{author.firstName} {author.lastName}</td>
+                </tr>
+            );
+        };
+
+        return (
+            <div>
+                <table className="table">
+                    <thead>
+                    <th>ID</th>
+                    <th>Name</th>
+                    </thead>
+                    <tbody>
+                    {this.props.authors.map(createAuthorRow, this)}
+                    </tbody>
+
+                </table>
+            </div>
+        );
+    }
+});
+
+module.exports = AuthorsList;
