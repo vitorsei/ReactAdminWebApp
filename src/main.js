6ebf703563c251cf8cbef9d5d263d0b1955400db
vitorsei <seiji.vitor@gmail.com>
2016-08-21 18:16:34 -0300

    Route Configuration

--- src/main.js
+++ src/main.js
@@ -1,37 +1,9 @@
-$ = jQuery = require('jquery');
+"use strict";
+
 var React = require('react');
-var Home = require('./components/homePage');
-var AuthorsPage = require('./components/authors/authorPage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win) {
-    "use strict";
-    var App = React.createClass({
-        render: function() {
-            var Child;
-
-            switch(this.props.route) {
-                case 'about': Child = About; break;
-                case 'authors': Child = AuthorsPage; break;
-                default: Child = Home;
-            }
-
-            return (
-                <div>
-                    <Header/>
-                    <Child/>
-                </div>
-            );
-
-        }
-    });
-
-    function render() {
-        var route = window.location.hash.substr(1);
-        React.render(<App route={route} />, document.getElementById('app'));
-    }
-
-    window.addEventListener('hashchange', render);
-    render();
-})(window);
+Router.run(routes, function(Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+});
