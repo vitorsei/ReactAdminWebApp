c3958af4c0094ffd6a2a04a2dfa3723d42dc5eb1
vitorsei <seiji.vitor@gmail.com>
2016-08-21 18:48:28 -0300

    Links

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+
 var Router = require('react-router');
 var routes = require('./routes');
 
--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,11 @@ var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 
 var routes = (
-  <Route name="app" path="/" handler={require('./components/app')}>
-    <DefaultRoute handler={require('./components/homePage')}/>
-    <Route name="authors" handler={require('./components/authorsPage')}/>
-    <Route name="about" handler={require('./components/aboutPage')}/>
-  </Route>
+    <Route name="app" path="/" handler={require('./components/app')}>
+        <DefaultRoute handler={require('./components/homePage')} />
+        <Route name="authors" handler={require('./components/authors/authorPage')} />
+        <Route name="about" handler={require('./components/about/aboutPage')} />
+    </Route>
 );
 
 module.exports = routes;
