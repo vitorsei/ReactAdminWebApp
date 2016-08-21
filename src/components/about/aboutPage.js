4f16486cb5c937c340df48103c1d26594a6185e2
vitorsei <seiji.vitor@gmail.com>
2016-08-21 19:07:08 -0300

    Transitions

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -6,6 +6,21 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if (!confirm('Are you sure you wanna read this page?')){
+                transition.abort();
+            }
+            else{
+                callback();
+            }
+        },
+        willTransitionFrom: function (transition, component) {
+            if (!confirm('Are you sure you wanna leave this page?')){
+                transition.abort();
+            }
+        }
+    },
     render: function () {
         return (
             <div>
