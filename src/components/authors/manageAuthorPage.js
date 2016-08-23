eefed3d1b2b221dff6abd6ed2df53b4301458af6
vitorsei <seiji.vitor@gmail.com>
2016-08-23 08:21:39 -0300

    Programmatic redirects

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,26 +4,41 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function () {
         return {
             author: { id: '', firstName: '', lastName: ''}
         };
     },
+
     setAuthorState: function (event) {
         var field = event.target.name;
         var value = event.target.value;
         this.state.author[field] = value;
         return this.setState({author: this.state.author});
     },
+
+    saveAuthor: function (event) {
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+        this.transitionTo('authors');
+    },
     
     render: function() {
         return (
             <AuthorForm
                 author={this.state.author}
-                onChange={this.setAuthorState} />
+                onChange={this.setAuthorState}
+                onSave={this.saveAuthor}
+            />
         );
     }
 });
